845f7be2875fedce89b93888dfb078a7a8c826ce
shamoon <4887959+shamoon@users.noreply.github.com>
2025-04-08 17:00:26 -0700

    Merge apps

--- src/documents/apps.py
+++ src/documents/apps.py
@@ -8,26 +8,4 @@ class DocumentsConfig(AppConfig):
     verbose_name = _("Documents")
 
     def ready(self):
-        from paperless.signals import document_consumption_finished
-        from paperless.signals import document_updated
-        from paperless.signals.handlers import add_inbox_tags
-        from paperless.signals.handlers import add_to_index
-        from paperless.signals.handlers import run_workflows_added
-        from paperless.signals.handlers import run_workflows_updated
-        from paperless.signals.handlers import set_correspondent
-        from paperless.signals.handlers import set_document_type
-        from paperless.signals.handlers import set_storage_path
-        from paperless.signals.handlers import set_tags
-
-        document_consumption_finished.connect(add_inbox_tags)
-        document_consumption_finished.connect(set_correspondent)
-        document_consumption_finished.connect(set_document_type)
-        document_consumption_finished.connect(set_tags)
-        document_consumption_finished.connect(set_storage_path)
-        document_consumption_finished.connect(add_to_index)
-        document_consumption_finished.connect(run_workflows_added)
-        document_updated.connect(run_workflows_updated)
-
-        import paperless.schema  # noqa: F401
-
         AppConfig.ready(self)
